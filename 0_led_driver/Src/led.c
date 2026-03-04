5e2cda6f8858a990cc69e117b4687821d1ea6c89
raghav414-ops <raghavkhanna1998@gmail.com>
2026-03-03 21:25:55 -0800

    0_LED_DRIVER headerfiles and main.c firstCommit

---
+++ 0_led_driver/Src/led.c
@@ -0,0 +1,23 @@
+#include "led.h"
+#define GPIOAEN (1U<<0)
+#define LED_PIN (1U<<5)
+void led_init(void)
+{
+	//enable clock access to led port
+	RCC->AHB1ENR |=GPIOAEN;
+	//set led pin as output pin
+	GPIOA->MODER |=(1U<<10);
+	GPIOA->MODER &= ~(1U<<11);
+}
+
+void led_on(void)
+{
+	//set led pin HIGH(PA5)
+	GPIOA->ODR |=LED_PIN;
+}
+
+void led_off(void)
+{
+	//set led pin LOW(PA5)
+	GPIOA->ODR &= ~LED_PIN;
+}
